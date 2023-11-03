c6696f049da89c066ae6ba123a1426c8a75a6d1a
miguel-merlin <mmerlin@stevens.edu>
2023-11-03 15:54:40 -0400

    Added add_user endpoint test

---
+++ user_management/add_user_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestAddUserHandler(t *testing.T) {
+	// Create a temporary users.yaml file for testing
+	tempFile, err := ioutil.TempFile("", "users.yaml")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer tempFile.Close()
+
+	// Write initial data to the temporary file
+	initialData := `
+users:
+  existinguser:
+    disabled: false
+    displayname: Existing User
+    password: existingpassword
+    email: existinguser@example.com
+    groups:
+      - group1
+`
+	err = ioutil.WriteFile(tempFile.Name(), []byte(initialData), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write initial data to temporary file: %v", err)
+	}
+
+	// Create a test request with the required parameters
+	formData := strings.NewReader("username=newuser&password=newpassword&displayname=New+User&email=newuser@example.com&groups=group2")
+	req := httptest.NewRequest(http.MethodPost, "/add-user", formData)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Create a response recorder to capture the response
+	res := httptest.NewRecorder()
+
+	// Call the addUserHandler function with the test request and response recorder
+	addUserHandler(res, req)
+
+	// Check the response status code
+	if res.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, res.Code)
+	}
+
+	// Read the updated users.yaml file
+	usersData, err := ioutil.ReadFile(tempFile.Name())
+	if err != nil {
+		t.Fatalf("Failed to read users.yaml file: %v", err)
+	}
+
+	// Assert that the new user is added to the users.yaml file
+	expectedData := `
+users:
+  existinguser:
+    disabled: false
+    displayname: Existing User
+    password: existingpassword
+    email: existinguser@example.com
+    groups:
+    - group1
+  newuser:
+    disabled: false
+    displayname: New User
+    password: newpassword
+    email: newuser@example.com
+    groups:
+    - group2
+`
+	if string(usersData) != expectedData {
+		t.Errorf("Expected users.yaml data:\n%s\n\nBut got:\n%s", expectedData, string(usersData))
+	}
+}
