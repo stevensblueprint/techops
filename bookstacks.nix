bc171d0beb248972d56f7313a97a01bfaa8987fc
Ezri Zhu <me@ezrizhu.com>
2024-02-23 12:16:06 -0500

    bookstacks smtp

--- bookstacks.nix
+++ bookstacks.nix
@@ -8,6 +8,14 @@ in
 		hostname = "wiki.sitblueprint.com";
 		appKeyFile = "/var/lib/bookstack/bookstack-appkey";
 		database.createLocally = true;
+		mail = {
+			host = "smtp.migadu.com";
+			port = 587;
+			user = "wiki@sitblueprint.com";
+			passwordFile = "/var/lib/bookstack/smtp-pass";
+			from = "wiki@sitblueprint.com";
+			fromName = "BlueprintWiki";
+		};
 		nginx = {
 			enableACME = true;
 			forceSSL = true;
